c23b508aafaa9a94dada71ce8fae4221f1fa4051
sushma <sushma.mareddy@gmail.com>
2023-02-25 22:21:29 +0530

    adding new student

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 		// TODO Auto-generated method stub
 		Student s1 = new Student(1,"sushma");
 		System.out.println(s1);
+		Student s2 = new Student(2,"sriya");
+		System.out.println(s2);
 
 	}
 
